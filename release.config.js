6b33d5d8e087d0a981b2b6ebba141da36890265b
Danilo Pianini <danilo.pianini@gmail.com>
2023-01-05 17:37:01 +0100

    fix(build): drop unused "uploadGit" task

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 var publishCmd = `
-./gradlew uploadKotlin uploadPluginMavenToMavenCentralNexus uploadGit --parallel || exit 1
+./gradlew uploadKotlin uploadPluginMavenToMavenCentralNexus release --parallel || exit 1
 ./gradlew publishPlugins -Pgradle.publish.key=$GRADLE_PUBLISH_KEY -Pgradle.publish.secret=$GRADLE_PUBLISH_SECRET || exit 2
 ./gradlew publishKotlinMavenPublicationToGithubRepository || true
 `
