39beca689d44ae203494d149c01a4f04e7fa0342
Danilo Pianini <danilo.pianini@unibo.it>
2023-02-19 11:29:10 +0100

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("com.gradle.enterprise") version "3.12.3"
+    id("org.danilopianini.gradle-pre-commit-git-hooks") version "1.1.2"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.4.0"
 }
 
 gradleEnterprise {
@@ -8,6 +10,14 @@ gradleEnterprise {
         termsOfServiceAgree = "yes"
         publishOnFailure()
     }
+}
+
+gitHooks {
+    preCommit {
+        tasks("ktlintCheck")
+    }
+    commitMsg { conventionalCommits() }
+    createHooks(true)
 }
 
 rootProject.name = "git-sensitive-semantic-versioning-gradle-plugin"
