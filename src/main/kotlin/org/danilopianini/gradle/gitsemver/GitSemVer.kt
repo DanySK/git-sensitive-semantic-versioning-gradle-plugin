5c27ff08b716803ec8c53b5cfc43d88a0c622c7f
Danilo Pianini <danilo.pianini@unibo.it>
2023-02-22 14:58:22 +0100

    fix: improve the forced version message

--- src/main/kotlin/org/danilopianini/gradle/gitsemver/GitSemVer.kt
+++ src/main/kotlin/org/danilopianini/gradle/gitsemver/GitSemVer.kt
@@ -20,7 +20,9 @@ class GitSemVer : Plugin<Project> {
                         require(SemanticVersion.semVerRegex.matches(it.toString())) {
                             "The version '$it' is not a valid semantic versioning format"
                         }
-                        project.logger.lifecycle("Forcing version to $it")
+                        project.logger.lifecycle(
+                            "Forcing version to $it, mandated by property '$forceVersionPropertyName'"
+                        )
                         project.version = it.toString()
                     } ?: run { assignGitSemanticVersion() }
                 }
