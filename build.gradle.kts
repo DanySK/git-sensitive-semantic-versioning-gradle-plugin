950fa629c2817e99bb275d74abd4d37056b59f9f
Danilo Pianini <danilo.pianini@unibo.it>
2023-10-19 12:05:31 +0200

    build(detekt): do not enforce the project's version of Kotlin upon detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -52,7 +52,7 @@ dependencies {
 }
 
 // Enforce Kotlin version coherence
-configurations.all {
+configurations.matching { it.name != "detekt" }.all {
     resolutionStrategy.eachDependency {
         if (requested.group == "org.jetbrains.kotlin" && requested.name.startsWith("kotlin")) {
             useVersion(KOTLIN_VERSION)
