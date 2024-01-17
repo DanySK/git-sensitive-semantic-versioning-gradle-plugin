1eebde634ead1ce17655a604959c56361526e103
Maksym Moroz <maksymmoroz@duck.com>
2024-01-17 17:31:37 +0200

    Revert "build(kotlin): enable context receivers via compiler args"
    This reverts commit 00fa18182061e3ad35654b428955a2e374148785.

--- build.gradle.kts
+++ build.gradle.kts
@@ -85,7 +85,7 @@ tasks {
     withType<KotlinCompile> {
         kotlinOptions {
             allWarningsAsErrors = true
-            freeCompilerArgs += listOf("-opt-in=kotlin.RequiresOptIn", "-Xinline-classes", "-Xcontext-receivers")
+            freeCompilerArgs += listOf("-opt-in=kotlin.RequiresOptIn", "-Xinline-classes")
         }
     }
 }
