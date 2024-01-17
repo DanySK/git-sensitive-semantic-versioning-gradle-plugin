f0e9388dc7f50e6631ebfdf469e50f5857eae1d1
Maksym Moroz <maksymmoroz@duck.com>
2024-01-17 17:29:36 +0200

    build(kotlin): enable context receivers via compiler args
    Signed-off-by: Maksym Moroz <maksymmoroz@duck.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -85,7 +85,7 @@ tasks {
     withType<KotlinCompile> {
         kotlinOptions {
             allWarningsAsErrors = true
-            freeCompilerArgs += listOf("-opt-in=kotlin.RequiresOptIn", "-Xinline-classes")
+            freeCompilerArgs += listOf("-opt-in=kotlin.RequiresOptIn", "-Xinline-classes", "-Xcontext-receivers")
         }
     }
 }
