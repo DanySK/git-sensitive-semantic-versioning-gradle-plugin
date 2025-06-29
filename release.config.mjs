4d34f2fb48e805c799cd9a7bcec2fcd87f50aa9b
Danilo Pianini <danilo.pianini@unibo.it>
2025-06-29 20:38:40 +0200

    fix(ci): migrate to Maven Central Portal

--- release.config.mjs
+++ release.config.mjs
@@ -1,5 +1,5 @@
 const publishCmd = `
-./gradlew uploadAllToMavenCentralNexus releaseStagingRepositoryOnMavenCentral --parallel || exit 1
+./gradlew publishAllPublicationsToProjectLocalRepository zipMavenCentralPortalPublication releaseMavenCentralPortalPublication || exit 1
 ./gradlew publishPlugins -Pgradle.publish.key=$GRADLE_PUBLISH_KEY -Pgradle.publish.secret=$GRADLE_PUBLISH_SECRET || exit 2
 ./gradlew publishKotlinMavenPublicationToGithubRepository || true
 `;
